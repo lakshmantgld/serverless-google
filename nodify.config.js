be2fed73a43bc99d7ef77b9d519db26e78397c71
diwaakar <diwaakar@Lakshmans-MacBook-Pro.local>
2018-03-05 09:19:57 +0900

    nodify changes & including package.json

--- nodify.config.js
+++ nodify.config.js
@@ -3,6 +3,7 @@ const minify = require('rollup-plugin-babel-minify');
 
 module.exports = {
   rollup: (config, env) => {
+    config.output.file = 'build/dist.js';
     if (env === 'production') {
       // using resolve as lambdas needs node_modules bundled up
       // and babel-minify is optional but just to reduce size as serverless has a size limit.
